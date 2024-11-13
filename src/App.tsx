02f8aebaf8520e9ef9a88403b9104097ebcd1c16
Le Dinh Quyen <quyenld9699@gmail.com>
2024-11-13 14:51:16 +0700

    update iconleap

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { TokenName } from '../lib/types';
 import { IconAndName } from '../lib/components';
 import { IconDOGE, IconEDU, IconFIL, IconKAS, IconKCS, IconROSE, IconTON, IconXMR } from '../lib/token';
 import './App.css';
+import { IconLeap } from '../lib/wallet';
 
 function App() {
     return (
@@ -9,6 +10,7 @@ function App() {
             <IconKCS sx={{ fontSize: '400px' }} />
             <IconAndName tokenName={TokenName.BTC} />
             <IconAndName tokenName={TokenName.BTC} reverse={true} />
+            <IconLeap sx={{ fontSize: '400px', borderRadius: '16px' }} />
         </div>
     );
 }
